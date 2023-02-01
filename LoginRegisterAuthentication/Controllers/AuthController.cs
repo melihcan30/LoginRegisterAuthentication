ecac31af19aefb0dc593a6ba273bbbb60653e796
Melihcan Serbest <canmelih.free3@gmail.com>
2023-02-01 16:16:24 +0300

    User Registration

---
+++ LoginRegisterAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using LoginRegisterAuthentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginRegisterAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+    }
+}
---
+++ LoginRegisterAuthentication/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LoginRegisterAuthentication.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; } = string.Empty;
+        public required string Passwor { get; set; } = string.Empty;
+    }
+}
